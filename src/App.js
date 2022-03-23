3299d4cb91a0e5b44a3457067155b9a304c7e988
jhe381697 <herlemont.josephine@gmail.com>
2022-03-23 17:39:32 +0100

    font awesome install

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <main className="App-main"></main>
-      <footer className="App-footer"></footer>
+      <header className="App-header">
+        <p>Navbar</p>
+      </header>
+      <main className="App-main">
+        <p>Joséphine Herlemont</p>
+      </main>
+      <footer className="App-footer">
+        <p>Copyright</p>
+      </footer>
     </div>
   );
 }
